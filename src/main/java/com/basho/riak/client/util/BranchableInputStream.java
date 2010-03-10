2b6c3953ddf7162478256dfb1496446059f910c0
jlee <none@none>
2010-03-10 09:28:10 -0500

    Add license headers

--- src/main/java/com/basho/riak/client/util/BranchableInputStream.java
+++ src/main/java/com/basho/riak/client/util/BranchableInputStream.java
@@ -1,3 +1,16 @@
+/*
+ * This file is provided to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.basho.riak.client.util;
 
 import java.io.IOException;
@@ -7,6 +20,12 @@ import java.nio.BufferUnderflowException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * An input stream that can be branched into other InputStreams, each
+ * maintaining its own location.
+ * 
+ * @author jlee <jonjlee@gmail.com>
+ */
 public class BranchableInputStream extends InputStream {
 
     static final int DEFAULT_BUFFER_SIZE = 4096;
