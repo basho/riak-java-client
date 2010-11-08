f6f1ed0363d4ef0368f70e83106bc25c7e0999c5
root <root@c3.(none)>
2010-11-08 14:30:22 -0600

    mapreduce builder tests

--- src/test/java/com/basho/riak/client/mapreduce/TestMapReduceBuilder.java
+++ src/test/java/com/basho/riak/client/mapreduce/TestMapReduceBuilder.java
@@ -28,6 +28,7 @@ import org.junit.Test;
 
 import com.basho.riak.client.RiakObject;
 import com.basho.riak.client.request.MapReduceBuilder;
+import com.basho.riak.client.mapreduce.filter.*;
 
 public class TestMapReduceBuilder {
 
@@ -249,4 +250,152 @@ public class TestMapReduceBuilder {
             "\"module\":\"riak_kv_mapreduce\",\"language\":\"erlang\",\"keep\":true,\"function\":" +
             "\"reduce_sort\"}}]}", json);
    }
+   
+   /*
+    * This test should always include all available key filters to ensure they render correctly.  This also
+    *  implicitly verifies overall key_filter construction.
+   */
+   @Test public void canBuildMapJobWithKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      List<String> setMembers = new ArrayList<String>();
+      setMembers.add("member1");
+      setMembers.add("member2");
+      setMembers.add("member3");
+      builder.keyFilter(new BetweenFilter(22, 93))
+             .keyFilter(new EndsWithFilter("apples"))
+             .keyFilter(new EqualToFilter(36))
+             .keyFilter(new FloatToStringFilter())
+             .keyFilter(new GreaterThanFilter(72))
+             .keyFilter(new GreaterThanOrEqualFilter(72))
+             .keyFilter(new IntToStringFilter())
+             .keyFilter(new LessThanFilter(42))
+             .keyFilter(new LessThanOrEqualFilter(43))
+             .keyFilter(new MatchFilter("fun.*"))
+             .keyFilter(new NotEqualToFilter(90))
+             .keyFilter(new SetMemberFilter(setMembers))
+             .keyFilter(new SimilarToFilter("valeu", 2))
+             .keyFilter(new StartsWithFilter("super"))
+             .keyFilter(new StringToFloatFilter())
+             .keyFilter(new StringToIntFilter())
+             .keyFilter(new TokenizeFilter("-", 3))
+             .keyFilter(new ToLowerFilter())
+             .keyFilter(new ToUpperFilter())
+             .keyFilter(new UrlDecodeFilter());
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"between\",22,93],[\"ends_with\",\"apples\"],[\"eq\",36],[\"float_to_string\"],[\"greater_than\",72],[\"greater_than_eq\",72],[\"int_to_string\"],[\"less_than\",42],[\"less_than_eq\",43],[\"matches\",\"fun.*\"],[\"neq\",90],[\"set_member\",\"member1\",\"member2\",\"member3\"],[\"similar_to\",\"valeu\",2],[\"starts_with\",\"super\"],[\"string_to_float\"],[\"string_to_int\"],[\"tokenize\",\"-\",3],[\"to_lower\"],[\"to_upper\"],[\"urldecode\"]],\"bucket\":\"wubba\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+
+   /*
+    * Test logical and, or, not
+   */
+
+   @Test public void canBuildMapJobWithBuilderLogicalOrKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new IntToStringFilter())
+             .keyFilter(new LogicalOrFilter().add(new BetweenFilter("apples", "bananas"))
+                                             .add(new GreaterThanFilter(36)))
+             .keyFilter(new BetweenFilter(12.5, 36.8));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"int_to_string\"],[\"or\",[\"between\",\"apples\",\"bananas\"],[\"greater_than\",36]],[\"between\",12.5,36.8]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+   
+   @Test public void canBuildMapJobWithVarArgLogicalOrKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new IntToStringFilter())
+             .keyFilter(new LogicalOrFilter(
+                new BetweenFilter("apples", "bananas"), new GreaterThanFilter(36)))
+             .keyFilter(new BetweenFilter(12.5, 36.8));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"int_to_string\"],[\"or\",[\"between\",\"apples\",\"bananas\"],[\"greater_than\",36]],[\"between\",12.5,36.8]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+
+   @Test public void canBuildMapJobWithBuilderLogicalAndKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new IntToStringFilter())
+             .keyFilter(new LogicalAndFilter().add(new BetweenFilter("apples", "bananas"))
+                                              .add(new GreaterThanFilter(36)))
+             .keyFilter(new BetweenFilter(12.5, 36.8));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"int_to_string\"],[\"and\",[\"between\",\"apples\",\"bananas\"],[\"greater_than\",36]],[\"between\",12.5,36.8]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+   
+   @Test public void canBuildMapJobWithVarArgLogicalAndKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new IntToStringFilter())
+             .keyFilter(new LogicalAndFilter(
+                new BetweenFilter("apples", "bananas"), new GreaterThanFilter(36)))
+             .keyFilter(new BetweenFilter(12.5, 36.8));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"int_to_string\"],[\"and\",[\"between\",\"apples\",\"bananas\"],[\"greater_than\",36]],[\"between\",12.5,36.8]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+
+   @Test public void canBuildMapJobWithBuilderLogicalNotKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new IntToStringFilter())
+             .keyFilter(new LogicalNotFilter().add(new BetweenFilter("apples", "bananas"))
+                                              .add(new GreaterThanFilter(36)))
+             .keyFilter(new BetweenFilter(12.5, 36.8));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"int_to_string\"],[\"not\",[\"between\",\"apples\",\"bananas\"],[\"greater_than\",36]],[\"between\",12.5,36.8]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+   
+   /*
+    * Test embedded LogicalFilterGroup (var args variation)
+   */
+   
+   @Test public void canBuildMapJobWithVarArgLogicalNotKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new IntToStringFilter(),
+                        new LogicalNotFilter(new BetweenFilter("apples", "bananas"),
+                                             new GreaterThanFilter(36)),
+                        new BetweenFilter(12.5, 36.8));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"int_to_string\"],[\"not\",[\"between\",\"apples\",\"bananas\"],[\"greater_than\",36]],[\"between\",12.5,36.8]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+
+   /*
+    * Test nested logical and, or, not
+   */
+   
+   @Test public void canBuildMapJobWithCompoundLogicalKeyFilters() throws JSONException {
+      MapReduceBuilder builder = new MapReduceBuilder();
+      builder.setBucket("wubba1");
+      builder.map(JavascriptFunction.named("Riak.mapValuesJson"), false);
+      builder.keyFilter(new LogicalOrFilter(
+        new LogicalOrFilter(
+            new LogicalFilterGroup(
+                new StringToIntFilter(),
+                new GreaterThanOrEqualFilter(98)),
+            new LogicalFilterGroup(
+                new StringToIntFilter(),
+                new LessThanFilter(2))),
+        new LogicalAndFilter(
+            new LogicalFilterGroup(
+                new StringToIntFilter(),
+                new BetweenFilter(18, 36)),
+            new LogicalFilterGroup(
+                new StringToIntFilter(),
+                new GreaterThanFilter(0))),
+        new LogicalNotFilter(
+            new LogicalFilterGroup(
+                new StringToIntFilter(),
+                new EqualToFilter(12)))));
+      String json = builder.toJSON().toString();
+      assertEquals("{\"inputs\":{\"key_filters\":[[\"or\",[\"or\",[[\"string_to_int\"],[\"greater_than_eq\",98]],[[\"string_to_int\"],[\"less_than\",2]]],[\"and\",[[\"string_to_int\"],[\"between\",18,36]],[[\"string_to_int\"],[\"greater_than\",0]]],[\"not\",[[\"string_to_int\"],[\"eq\",12]]]]],\"bucket\":\"wubba1\"},\"query\":[{\"map\":{\"name\":\"Riak.mapValuesJson\",\"language\":\"javascript\",\"keep\":false}}]}", json);
+   }
+   
 }
