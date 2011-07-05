c251928ef12cde7c1f7760fdeac36d01a77ae5a1
Russell Brown <russell.brown@mac.com>
2011-07-05 12:48:06 +0100

    Remove redundant imports

--- src/main/java/com/basho/riak/pbc/RiakConnectionPool.java
+++ src/main/java/com/basho/riak/pbc/RiakConnectionPool.java
@@ -16,7 +16,6 @@ package com.basho.riak.pbc;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Arrays;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
