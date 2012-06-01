e3a227951f60517ac11ddf2816915d832306f04f
guido-medina <guido.medina@temetra.com>
2012-06-01 19:27:33 +0100

    A minor change, type was already known

--- src/main/java/com/basho/riak/client/convert/reflect/AnnotationInfo.java
+++ src/main/java/com/basho/riak/client/convert/reflect/AnnotationInfo.java
@@ -209,7 +209,7 @@ public class AnnotationInfo {
 
         for (RiakIndexMethod m : indexMethods) {
             if (Set.class.isAssignableFrom(m.getType())) {
-                Type t = m.getMethod().getGenericReturnType();
+                Type t = m.getType();
                 if (t instanceof ParameterizedType) {
                     Class<?> genericType = (Class<?>) ((ParameterizedType) t).getActualTypeArguments()[0];
                     if (String.class.equals(genericType)) {
