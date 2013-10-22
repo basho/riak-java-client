5f23c5beefab701c4d3110c64f44b33c52d968f9
Dave Rusek <dave.rusek@gmail.com>
2013-10-22 10:59:59 -0600

    Update SearchOperation to user builders

--- src/main/java/com/basho/riak/client/core/operations/SearchOperation.java
+++ src/main/java/com/basho/riak/client/core/operations/SearchOperation.java
@@ -24,7 +24,7 @@ import com.basho.riak.protobuf.RiakPB.RpbPair;
 import com.basho.riak.protobuf.RiakSearchPB;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
-import java.util.Collections;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -40,166 +40,34 @@ import java.util.concurrent.ExecutionException;
  * <p>
  * This operation will fail if search is not enabled or the index does not exist.
  * </p>
+ *
  * @author Brian Roach <roach at basho dot com>
  * @since 2.0
  */
 public class SearchOperation extends FutureOperation<SearchResult, RiakSearchPB.RpbSearchQueryResp>
 {
 
-    private final RiakSearchPB.RpbSearchQueryReq.Builder builder =
-        RiakSearchPB.RpbSearchQueryReq.newBuilder();
-    
-    /**
-     * Constructs a SearchOperation for Riak Search or Yokozuna.
-     * In the case of Yokozuna the index name is required to be UTF-8.
-     * @param indexName the index name
-     * @param queryString the query string
-     */
-    public SearchOperation(ByteArrayWrapper indexName, String queryString)
+    private final ByteArrayWrapper indexName;
+    private final String queryString;
+    private final RiakSearchPB.RpbSearchQueryReq.Builder reqBuilder;
+
+    private SearchOperation(Builder builder)
     {
-        if (null == indexName || indexName.length() == 0)
-        {
-            throw new IllegalArgumentException("Index name cannot be null or zero length");
-        }
-        if (null == queryString || queryString.length() == 0)
-        {
-            throw new IllegalArgumentException("Query string cannot be null or zero length");
-        }
-        
-        builder.setIndex(ByteString.copyFrom(indexName.unsafeGetValue()));
-        builder.setQ(ByteString.copyFromUtf8(queryString));
+        this.queryString = builder.queryString;
+        this.indexName = builder.indexName;
+        this.reqBuilder = builder.reqBuilder;
     }
-    
-    /**
-     * Specify the maximum number of results to return.
-     * Riak defaults to 10 if this is not set.
-     * @param rows the maximum number of results to return.
-     * @return a reference to this object.
-     */
-    public SearchOperation withNumRows(int rows)
-    {
-        if (rows <= 0)
-        {
-            throw new IllegalArgumentException("Rows must be >= 1");
-        }
-        builder.setRows(rows);
-        return this;
-    }
-    
-    /**
-     * Specify the starting result of the query.
-     * Useful for pagination. The default is 0.
-     * @param start the index of the starting result.
-     * @return a reference to this object. 
-     */
-    public SearchOperation withStart(int start)
-    {
-        if (start < 0)
-        {
-            throw new IllegalArgumentException("Start must be >= 0");
-        }
-        builder.setStart(start);
-        return this;
-    }
-    
-    /**
-     * Sort the results on the specified field name.
-     * Default is “none”, which causes the results to be sorted in descending order by score.
-     * @param fieldName the fieldname to sort the results on.
-     * @return a reference to this object. 
-     */
-    public SearchOperation withSortField(String fieldName)
-    {
-        stringCheck(fieldName);
-        builder.setSort(ByteString.copyFromUtf8(fieldName));
-        return this;
-    }
-    
-    /**
-     * Filters the search by an additional query scoped to inline fields.
-     * @param filterQuery the filter query.
-     * @return a reference to this object. 
-     */
-    public SearchOperation withFilterQuery(String filterQuery)
-    {
-        stringCheck(filterQuery);
-        builder.setFilter(ByteString.copyFromUtf8(filterQuery));
-        return this;
-    }
-    
-    /**
-     * Use the provided field as the default.
-     * Overrides the “default_field” setting in the schema file.
-     * @param fieldName the name of the field.
-     * @return a reference to this object. 
-     */
-    public SearchOperation withDefaultField(String fieldName)
-    {
-        stringCheck(fieldName);
-        builder.setDf(ByteString.copyFromUtf8(fieldName));
-        return this;
-    }
-    
-    /**
-     * Set the default operation.
-     * Allowed settings are either “and” or “or”. 
-     * Overrides the “default_op” setting in the schema file. 
-     * @param op A string containing either "and" or "or".
-     * @return a reference to this object. 
-     */
-    public SearchOperation withDefaultOperation(String op)
-    {
-        stringCheck(op);
-        builder.setOp(ByteString.copyFromUtf8(op));
-        return this;
-    }
-    
-    /**
-     * Only return the provided fields.
-     * Filters the results to only contain the provided fields.
-     * @param fields a list of field names.
-     * @return a reference to this object. 
-     */
-    public SearchOperation withReturnFields(List<String> fields)
-    {
-        for (String f : fields)
-        {
-            stringCheck(f);
-            builder.addFl(ByteString.copyFromUtf8(f));
-        }
-        return this;
-    }
-    
-    /**
-     * Sorts all of the results by bucket key, or the search score, before the given rows are chosen.
-     * This is useful when paginating to ensure the results are returned in a consistent order.
-     * @param presort a String containing either "key" or "score".
-     * @return a reference to this object. 
-     */
-    public SearchOperation withPresort(String presort)
-    {
-        stringCheck(presort);
-        builder.setPresort(ByteString.copyFromUtf8(presort));
-        return this;
-    }
-    
-    private void stringCheck(String arg)
-    {
-        if (null == arg || arg.length() == 0)
-        {
-            throw new IllegalArgumentException("Arguemt cannot be null or zero length");
-        }
-    }
-    
+
+
     @Override
     protected SearchResult convert(List<RiakSearchPB.RpbSearchQueryResp> rawResponse) throws ExecutionException
     {
         // This isn't a streaming op, there will only be one protobuf
         RiakSearchPB.RpbSearchQueryResp resp = rawResponse.get(0);
-        List<Map<String,String>> docList = new LinkedList<Map<String,String>>();
+        List<Map<String, String>> docList = new LinkedList<Map<String, String>>();
         for (RiakSearchPB.RpbSearchDoc pbDoc : resp.getDocsList())
         {
-            Map<String,String> map = new HashMap<String,String>();
+            Map<String, String> map = new HashMap<String, String>();
             for (RpbPair pair : pbDoc.getFieldsList())
             {
                 map.put(pair.getKey().toStringUtf8(), pair.getValue().toStringUtf8());
@@ -207,13 +75,13 @@ public class SearchOperation extends FutureOperation<SearchResult, RiakSearchPB.
             docList.add(map);
         }
         return new SearchResult(docList, resp.getMaxScore(), resp.getNumFound());
-        
+
     }
 
     @Override
     protected RiakMessage createChannelMessage()
     {
-        RiakSearchPB.RpbSearchQueryReq req = builder.build();
+        RiakSearchPB.RpbSearchQueryReq req = reqBuilder.build();
         return new RiakMessage(RiakMessageCodes.MSG_SearchQueryReq, req.toByteArray());
     }
 
@@ -230,5 +98,165 @@ public class SearchOperation extends FutureOperation<SearchResult, RiakSearchPB.
             throw new IllegalArgumentException("Invalid message received", ex);
         }
     }
-    
+
+    public static class Builder
+    {
+
+        private final ByteArrayWrapper indexName;
+        private final String queryString;
+        private final RiakSearchPB.RpbSearchQueryReq.Builder reqBuilder =
+            RiakSearchPB.RpbSearchQueryReq.newBuilder();
+
+        public Builder(ByteArrayWrapper indexName, String queryString)
+        {
+            if (null == indexName || indexName.length() == 0)
+            {
+                throw new IllegalArgumentException("Index name cannot be null or zero length");
+            }
+            if (null == queryString || queryString.length() == 0)
+            {
+                throw new IllegalArgumentException("Query string cannot be null or zero length");
+            }
+
+            this.indexName = indexName;
+            this.queryString = queryString;
+            reqBuilder.setIndex(ByteString.copyFrom(indexName.unsafeGetValue()));
+            reqBuilder.setQ(ByteString.copyFromUtf8(queryString));
+        }
+
+        /**
+         * Specify the maximum number of results to return.
+         * Riak defaults to 10 if this is not set.
+         *
+         * @param rows the maximum number of results to return.
+         * @return a reference to this object.
+         */
+        public Builder withNumRows(int rows)
+        {
+            if (rows <= 0)
+            {
+                throw new IllegalArgumentException("Rows must be >= 1");
+            }
+            reqBuilder.setRows(rows);
+            return this;
+        }
+
+        /**
+         * Specify the starting result of the query.
+         * Useful for pagination. The default is 0.
+         *
+         * @param start the index of the starting result.
+         * @return a reference to this object.
+         */
+        public Builder withStart(int start)
+        {
+            if (start < 0)
+            {
+                throw new IllegalArgumentException("Start must be >= 0");
+            }
+            reqBuilder.setStart(start);
+            return this;
+        }
+
+        /**
+         * Sort the results on the specified field name.
+         * Default is “none”, which causes the results to be sorted in descending order by score.
+         *
+         * @param fieldName the fieldname to sort the results on.
+         * @return a reference to this object.
+         */
+        public Builder withSortField(String fieldName)
+        {
+            stringCheck(fieldName);
+            reqBuilder.setSort(ByteString.copyFromUtf8(fieldName));
+            return this;
+        }
+
+        /**
+         * Filters the search by an additional query scoped to inline fields.
+         *
+         * @param filterQuery the filter query.
+         * @return a reference to this object.
+         */
+        public Builder withFilterQuery(String filterQuery)
+        {
+            stringCheck(filterQuery);
+            reqBuilder.setFilter(ByteString.copyFromUtf8(filterQuery));
+            return this;
+        }
+
+        /**
+         * Use the provided field as the default.
+         * Overrides the “default_field” setting in the schema file.
+         *
+         * @param fieldName the name of the field.
+         * @return a reference to this object.
+         */
+        public Builder withDefaultField(String fieldName)
+        {
+            stringCheck(fieldName);
+            reqBuilder.setDf(ByteString.copyFromUtf8(fieldName));
+            return this;
+        }
+
+        /**
+         * Set the default operation.
+         * Allowed settings are either “and” or “or”.
+         * Overrides the “default_op” setting in the schema file.
+         *
+         * @param op A string containing either "and" or "or".
+         * @return a reference to this object.
+         */
+        public Builder withDefaultOperation(String op)
+        {
+            stringCheck(op);
+            reqBuilder.setOp(ByteString.copyFromUtf8(op));
+            return this;
+        }
+
+        /**
+         * Only return the provided fields.
+         * Filters the results to only contain the provided fields.
+         *
+         * @param fields a list of field names.
+         * @return a reference to this object.
+         */
+        public Builder withReturnFields(List<String> fields)
+        {
+            for (String f : fields)
+            {
+                stringCheck(f);
+                reqBuilder.addFl(ByteString.copyFromUtf8(f));
+            }
+            return this;
+        }
+
+        /**
+         * Sorts all of the results by bucket key, or the search score, before the given rows are chosen.
+         * This is useful when paginating to ensure the results are returned in a consistent order.
+         *
+         * @param presort a String containing either "key" or "score".
+         * @return a reference to this object.
+         */
+        public Builder withPresort(String presort)
+        {
+            stringCheck(presort);
+            reqBuilder.setPresort(ByteString.copyFromUtf8(presort));
+            return this;
+        }
+
+        private void stringCheck(String arg)
+        {
+            if (null == arg || arg.length() == 0)
+            {
+                throw new IllegalArgumentException("Arguemt cannot be null or zero length");
+            }
+        }
+
+        public SearchOperation build()
+        {
+            return new SearchOperation(this);
+        }
+    }
+
 }
