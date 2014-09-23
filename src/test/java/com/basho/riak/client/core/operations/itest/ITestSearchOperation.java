6af2813bdead186250c9d120781a1167e8b785fa
Brian Roach <roach@basho.com>
2014-09-23 14:19:20 -0600

    Modified search itest for field multivalue.
    The itest confirms the fix works by indexing a JSON object with
    "multi_ss" : ["this", "that"] and going a search for multi_ss:t*
    
    The returned response contains both this and that mapped to the
    "multi_ss" key

--- src/test/java/com/basho/riak/client/core/operations/itest/ITestSearchOperation.java
+++ src/test/java/com/basho/riak/client/core/operations/itest/ITestSearchOperation.java
@@ -102,9 +102,13 @@ public class ITestSearchOperation extends ITestBase
             assertTrue(propsOp.cause().toString(), propsOp.isSuccess());
         }
         
+        Thread.sleep(5000);
+        
         prepSearch(yokozunaBucketType, searchBucket);
         
-        SearchOperation searchOp = new SearchOperation.Builder(BinaryValue.create("test_index"), "text:Alice*").build();
+        Thread.sleep(5000); 
+        
+        SearchOperation searchOp = new SearchOperation.Builder(BinaryValue.create("test_index"), "multi_ss:t*").build();
         
         cluster.execute(searchOp);
         searchOp.await();
@@ -113,7 +117,9 @@ public class ITestSearchOperation extends ITestBase
         for (Map<String, List<String>> map : result.getAllResults())
         {
             assertFalse(map.isEmpty());
-            assertEquals(5, map.size()); // {_yz_rk=p3, _yz_rb=search_bucket, _yz_rt=default, score=1.00000000000000000000e+00, _yz_id=default_search_bucket_p3_25}
+            assertEquals(7, map.size()); // [_yz_rk, _yz_rb, multi_ss, content_s, _yz_rt, score, _yz_id]
+            assertTrue(map.containsKey("multi_ss"));
+            assertEquals(2, map.get("multi_ss").size()); 
         }
         
         resetAndEmptyBucket(namespace);
@@ -128,13 +134,13 @@ public class ITestSearchOperation extends ITestBase
     private void prepSearch(BinaryValue searchBucketType, BinaryValue searchBucket) throws InterruptedException, ExecutionException
     {
 
-        RiakObject obj = new RiakObject();
+        RiakObject obj = new RiakObject().setContentType("application/json");
                             
-        obj.setValue(BinaryValue.create("Alice was beginning to get very tired of sitting by her sister on the " +
+        obj.setValue(BinaryValue.create("{ \"content_s\":\"Alice was beginning to get very tired of sitting by her sister on the " +
                     "bank, and of having nothing to do: once or twice she had peeped into the " +
                     "book her sister was reading, but it had no pictures or conversations in " +
                     "it, 'and what is the use of a book,' thought Alice 'without pictures or " +
-                    "conversation?'"));
+                    "conversation?'\"}"));
 
         Namespace namespace = new Namespace(searchBucketType, searchBucket);
         Location location = new Location(namespace, BinaryValue.unsafeCreate("p1".getBytes()));
@@ -146,13 +152,13 @@ public class ITestSearchOperation extends ITestBase
         cluster.execute(storeOp);
         storeOp.get();
         
-        obj.setValue(BinaryValue.create("So she was considering in her own mind (as well as she could, for the " +
+        obj.setValue(BinaryValue.create("{ \"content_s\":\"So she was considering in her own mind (as well as she could, for the " +
                     "hot day made her feel very sleepy and stupid), whether the pleasure " +
                     "of making a daisy-chain would be worth the trouble of getting up and " +
                     "picking the daisies, when suddenly a White Rabbit with pink eyes ran " +
-                    "close by her."));
+                    "close by her.\", \"multi_ss\":[\"this\",\"that\"]}"));
         
-        obj.setContentType("text/plain");
+        
         location = new Location(namespace, BinaryValue.unsafeCreate("p2".getBytes()));
         storeOp = 
             new StoreOperation.Builder(location)
@@ -162,12 +168,11 @@ public class ITestSearchOperation extends ITestBase
         cluster.execute(storeOp);
         storeOp.get();
         
-        obj.setValue(BinaryValue.create("The rabbit-hole went straight on like a tunnel for some way, and then " +
+        obj.setValue(BinaryValue.create("{ \"content_s\":\"The rabbit-hole went straight on like a tunnel for some way, and then " +
                     "dipped suddenly down, so suddenly that Alice had not a moment to think " +
                     "about stopping herself before she found herself falling down a very deep " +
-                    "well."));
+                    "well.\"}"));
         
-        obj.setContentType("text/plain");
         location = new Location(namespace, BinaryValue.unsafeCreate("p3".getBytes()));
         storeOp = 
             new StoreOperation.Builder(location)
