a0747a985d608a53d726e14d9dee9433216bbd63
Sergey Galkin <srggal@gmail.com>
2015-08-26 11:49:53 +0300

    LICENSE

--- src/main/java/com/basho/riak/client/core/operations/CoveragePlanOperation.java
+++ src/main/java/com/basho/riak/client/core/operations/CoveragePlanOperation.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013-2015 Basho Technologies Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.basho.riak.client.core.operations;
 
 import com.basho.riak.client.core.FutureOperation;
@@ -12,6 +27,12 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import java.io.Serializable;
 import java.util.*;
 
+// TODO: documentation for CoveragePlanOperation needs to be improved
+/**
+ * An operation to retrieve a coverage plan from Riak.
+ *
+ * @author Sergey Galkin <sgalkin at basho dot com>
+ */
 public class CoveragePlanOperation extends FutureOperation<CoveragePlanOperation.Response, RiakKvPB.RpbCoverageResp, Namespace> {
     private final RiakKvPB.RpbCoverageReq.Builder reqBuilder;
     private final Namespace namespace;
