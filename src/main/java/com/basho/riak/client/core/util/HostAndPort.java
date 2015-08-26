a947d4a74b73d05f2cf6eb24a0c088888011a860
Sergey Galkin <srggal@gmail.com>
2015-08-26 11:49:53 +0300

    LICENSE

--- src/main/java/com/basho/riak/client/core/util/HostAndPort.java
+++ src/main/java/com/basho/riak/client/core/util/HostAndPort.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Basho Technologies Inc.
+ * Copyright 2013-2015 Basho Technologies Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
