68f314a4642596c8ec345448cf53bd9e695550d6
Alex Moore <moore.alex@gmail.com>
2015-09-24 16:57:26 -0400

    adding a few initializer tests

--- src/main/java/com/basho/riak/client/core/util/DefaultCharset.java
+++ src/main/java/com/basho/riak/client/core/util/DefaultCharset.java
@@ -8,41 +8,42 @@ import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * <p>
- *     Holds an classloader-wide default charset, that
- *     is then used to encode/decode between Strings and
- *     Byte arrays for Riak's use (see {@link com.basho.riak.client.core.util.BinaryValue}).
- *</p>
- *
+ * Holds an classloader-wide default charset, that
+ * is then used to encode/decode between Strings and
+ * Byte arrays for Riak's use (see {@link com.basho.riak.client.core.util.BinaryValue}).
+ * </p>
+ * <p/>
  * <p>
- *     Before 2.0.3, the system used the JRE's default Charset from the
- *     {@link Charset#defaultCharset()} property.
- *</p>
- *
+ * Before 2.0.3, the system used the JRE's default Charset from the
+ * {@link Charset#defaultCharset()} property.
+ * </p>
+ * <p/>
  * <p>
- *     With this class you may change the Riak client to use a different default.
- *     You can set it at startup by providing the desired Charset name with the vm argument
- *     {@code -Dcom.basho.riak.client.DefaultCharset="UTF-8"}, or at runtime
- *     with the static method (see {@link #set(Charset)}).
+ * With this class you may change the Riak client to use a different default.
+ * You can set it at startup by providing the desired Charset name with the vm argument
+ * {@code -Dcom.basho.riak.client.DefaultCharset="UTF-8"}, or at runtime
+ * with the static method (see {@link #set(Charset)}).
+ * </p>
+ * <p/>
+ * <p>
+ * As of 2.0.3 it still defaults to the value provided by
+ * {@link Charset#defaultCharset()},
+ * but the default is planned to change to "UTF-8" with 2.1.0.
+ * </p>
+ * <p/>
+ * <p>
+ * If your JRE default charset is one of "<b>US-ASCII</b>",
+ * "<b>UTF-8</b>", or "<b>ISO-8859-1</b>",
+ * this change should <b>not</b> affect you.
+ * </p>
+ * <p/>
+ * <p>
+ * If your JRE default charset is one of "<b>UTF-16</b>",
+ * "<b>UTF-16BE</b>", or "<b>UTF-16LE</b>",
+ * <b>you will need to set that default on the command line or
+ * after application startup once you upgrade</b>.
  * </p>
  *
- * <p>
- *     As of 2.0.3 it still defaults to the value provided by
- *     {@link Charset#defaultCharset()},
- *     but the default is planned to change to "UTF-8" with 2.1.0.
- * </p>
- *
- * <p>
- *     If your JRE default charset is one of "<b>US-ASCII</b>",
- *     "<b>UTF-8</b>", or "<b>ISO-8859-1</b>",
- *     this change should <b>not</b> affect you.
- * </p>
- *
- * <p>
- *     If your JRE default charset is one of "<b>UTF-16</b>",
- *     "<b>UTF-16BE</b>", or "<b>UTF-16LE</b>",
- *     <b>you will need to set that default on the command line or
- *     after application startup once you upgrade</b>.
- * </p>
  * @author Alex Moore <amoore AT basho dot com>
  * @since 2.0.3
  */
@@ -66,7 +67,7 @@ public final class DefaultCharset
         final Charset systemDefault = Charset.defaultCharset();
         final String declaredCharsetName = System.getProperty(Constants.CLIENT_OPTION_CHARSET);
 
-        if(declaredCharsetName != null && !declaredCharsetName.isEmpty())
+        if (declaredCharsetName != null && !declaredCharsetName.isEmpty())
         {
             try
             {
@@ -82,7 +83,8 @@ public final class DefaultCharset
         }
         else
         {
-            logger.info("No declared charset found, the default charset '{}' will be used", systemDefault.name());
+            logger.info("No desired charset found in system properties, the default charset '{}' will be used",
+                        systemDefault.name());
             charset = systemDefault;
         }
 
@@ -91,6 +93,7 @@ public final class DefaultCharset
 
     /**
      * Get the current classloader-wide default Charset for the Riak client.
+     *
      * @return The current classloader-wide default charset.
      */
     public static Charset get()
@@ -100,6 +103,7 @@ public final class DefaultCharset
 
     /**
      * Set the classloader-wide default Charset for the Riak client.
+     *
      * @param charset The charset to set the classloader-wide default to.
      */
     public static void set(Charset charset)
--- src/test/java/com/basho/riak/client/core/util/DefaultCharsetTest.java
+++ src/test/java/com/basho/riak/client/core/util/DefaultCharsetTest.java
@@ -1,28 +1,123 @@
 package com.basho.riak.client.core.util;
 
-import java.nio.charset.Charset;
-
+import org.junit.Assume;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertEquals;
 
 public class DefaultCharsetTest
 {
     @Test
-    public void testDefaults()
+    public void testDefaultIfNoPropertyDefined()
     {
         Charset systemDefault = Charset.defaultCharset();
         Charset clientDefault = DefaultCharset.get();
 
         String definedCharsetName = System.getProperty(Constants.CLIENT_OPTION_CHARSET);
 
-        if(definedCharsetName == null || definedCharsetName.isEmpty())
+        Boolean noCharsetPropertyDefined = definedCharsetName == null;
+        Assume.assumeTrue(noCharsetPropertyDefined);
+
+        assertEquals(systemDefault, clientDefault);
+    }
+
+    @Test
+    public void testDefinedCharset()
+    {
+        Charset clientDefault = DefaultCharset.get();
+
+        String definedCharsetName = System.getProperty(Constants.CLIENT_OPTION_CHARSET);
+
+        Boolean charsetPropertyDefined = definedCharsetName != null && !definedCharsetName.isEmpty();
+
+        Assume.assumeTrue(charsetPropertyDefined);
+
+        Charset definedCharset = Charset.forName(definedCharsetName);
+        assertEquals(definedCharset, clientDefault);
+    }
+
+    @Test
+    public void testStaticInitializerValidDefinedCharset()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException
+    {
+        String definedCharsetName = System.getProperty(Constants.CLIENT_OPTION_CHARSET);
+        Charset systemDefault = Charset.defaultCharset();
+        String goodCharsetName = "UTF-16";
+
+        try
         {
-            assertEquals(systemDefault, clientDefault);
+            Charset goodCharset = Charset.forName(goodCharsetName);
+
+            // Open up the private static initializer
+            Method staticInitializer = DefaultCharset.class.getDeclaredMethod("initializeDefaultCharsetSingleton",
+                                                                              null);
+            staticInitializer.setAccessible(true);
+
+            // Set system property for client charset to bad value
+            System.setProperty(Constants.CLIENT_OPTION_CHARSET, goodCharsetName);
+
+            // Run static initializer to test good charset handling
+            DefaultCharset initializedDefaultCharset = (DefaultCharset) staticInitializer.invoke(null);
+
+            // Get initialized charset
+            Field currentCharsetField = DefaultCharset.class.getDeclaredField("currentCharset");
+            currentCharsetField.setAccessible(true);
+            AtomicReference<Charset> initializedCharset =
+                    (AtomicReference<Charset>)currentCharsetField.get(initializedDefaultCharset);
+
+            assertEquals(goodCharset.name(), initializedCharset.get().name());
         }
-        else
+        finally
         {
-            Charset definedCharset = Charset.forName(definedCharsetName);
-            assertEquals(definedCharset, clientDefault);
+            // Cleanup
+            if (definedCharsetName != null)
+            {
+                System.setProperty(Constants.CLIENT_OPTION_CHARSET, definedCharsetName);
+            }
+        }
+    }
+
+    @Test
+    public void testStaticInitializerInvalidDefinedCharset()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException
+    {
+        String definedCharsetName = System.getProperty(Constants.CLIENT_OPTION_CHARSET);
+        Charset systemDefault = Charset.defaultCharset();
+        String badCharsetName = "Foobar-16NE";
+
+        try
+        {
+            // Open up the private static initializer
+            Method staticInitializer = DefaultCharset.class.getDeclaredMethod("initializeDefaultCharsetSingleton",
+                                                                              null);
+            staticInitializer.setAccessible(true);
+
+            // Set system property for client charset to bad value
+            System.setProperty(Constants.CLIENT_OPTION_CHARSET, badCharsetName);
+
+            // Run static initializer to test bad charset handling
+            DefaultCharset initializedDefaultCharset = (DefaultCharset) staticInitializer.invoke(null);
+
+            Field currentCharsetField = DefaultCharset.class.getDeclaredField("currentCharset");
+            currentCharsetField.setAccessible(true);
+            AtomicReference<Charset> initializedCharset =
+                    (AtomicReference<Charset>)currentCharsetField.get(initializedDefaultCharset);
+
+            assertEquals(systemDefault.name(), initializedCharset.get().name());
+        }
+        finally
+        {
+            // Cleanup
+            if (definedCharsetName != null)
+            {
+                System.setProperty(Constants.CLIENT_OPTION_CHARSET, definedCharsetName);
+            }
         }
     }
 
@@ -33,7 +128,7 @@ public class DefaultCharsetTest
         Charset ascii = Charset.forName("US-ASCII");
         Charset utf8 = Charset.forName("UTF-8");
 
-        if(systemDefault != utf8)
+        if (systemDefault != utf8)
         {
             DefaultCharset.set(utf8);
             Charset clientCharset = DefaultCharset.get();
