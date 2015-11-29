3b3574964e5523eed3d3fb21a93cccb2ddc43436
Sergey Galkin <srggal@gmail.com>
2015-11-29 07:11:19 +0200

    Introduce QueryResult.EMPTY

--- src/main/java/com/basho/riak/client/core/converters/TimeSeriesPBConverter.java
+++ src/main/java/com/basho/riak/client/core/converters/TimeSeriesPBConverter.java
@@ -24,7 +24,7 @@ public final class TimeSeriesPBConverter
     {
         if (response == null)
         {
-            return QueryResult.emptyResult();
+            return QueryResult.EMPTY;
         }
 
         final List<ColumnDescription> columnDescriptions = convertPBColumnDescriptions(response.getColumnsList());
@@ -37,7 +37,7 @@ public final class TimeSeriesPBConverter
     {
         if (response == null)
         {
-            return QueryResult.emptyResult();
+            return QueryResult.EMPTY;
         }
 
         final List<ColumnDescription> columnDescriptions = convertPBColumnDescriptions(response.getColumnsList());
