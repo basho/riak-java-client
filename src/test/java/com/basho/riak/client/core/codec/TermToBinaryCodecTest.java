30a676f4b95ac9b24849d5982ca621c390093454
Luke Bakken <luke@bakken.io>
2016-04-21 08:54:22 -0700

    Add a TsPutReq test

--- src/test/java/com/basho/riak/client/core/codec/TermToBinaryCodecTest.java
+++ src/test/java/com/basho/riak/client/core/codec/TermToBinaryCodecTest.java
@@ -6,8 +6,7 @@ import com.ericsson.otp.erlang.OtpOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
+import java.util.Calendar;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,6 +18,83 @@ public class TermToBinaryCodecTest
 {
     private static final String TABLE_NAME = "test_table";
     private static final String QUERY = "SELECT * FROM FRAZZLE";
+
+    @Test
+    public void encodesPutRequestCorrectly_1() {
+        // {tsputreq, <<"test_table">>, [], [{<<"varchar">>, 12345678, 12.34, true, 12345}, {<<"string">>, 8765432, 43.21, false, 543321}]}
+        final byte[] exp = {(byte)131, 104, 4, 100, 0, 8, 116, 115, 112, 117, 116, 114, 101, 113, 109, 0,
+            0, 0, 10, 116, 101, 115, 116, 95, 116, 97, 98, 108, 101, 106, 108, 0, 0,
+            0, 2, 104, 5, 109, 0, 0, 0, 7, 118, 97, 114, 99, 104, 97, 114, 98, 0, (byte)188,
+            97, 78, 99, 49, 46, 50, 51, 51, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57,
+            57, 57, 57, 56, 53, 55, 57, 101, 43, 48, 49, 0, 0, 0, 0, 0, 100, 0, 4, 116,
+            114, 117, 101, 98, 0, 0, 48, 57, 104, 5, 109, 0, 0, 0, 6, 115, 116, 114,
+            105, 110, 103, 98, 0, (byte)133, (byte)191, (byte)248, 99, 52, 46, 51, 50, 49, 48, 48, 48,
+            48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 56, 53, 50, 55, 101, 43, 48, 49,
+            0, 0, 0, 0, 0, 100, 0, 5, 102, 97, 108, 115, 101, 98, 0, 8, 74, 89, 106};
+
+        Cell c1 = new Cell("varchar");
+        Cell c2 = new Cell(12345678L);
+        Cell c3 = new Cell(12.34);
+        Cell c4 = new Cell(true);
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTimeInMillis(12345);
+        Cell c5 = new Cell(cal1);
+        Row r1 = new Row(c1, c2, c3, c4, c5);
+
+        Cell c6 = new Cell("string");
+        Cell c7 = new Cell(8765432L);
+        Cell c8 = new Cell(43.21);
+        Cell c9 = new Cell(false);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTimeInMillis(543321);
+        Cell c10 = new Cell(cal2);
+        Row r2 = new Row(c6, c7, c8, c9, c10);
+        Row[] rows = { r1, r2 };
+
+        try {
+            OtpOutputStream os = TermToBinaryCodec.encodeTsPutRequest(TABLE_NAME, Arrays.asList(rows));
+            os.flush();
+            byte[] msg = os.toByteArray();
+            Assert.assertArrayEquals(exp, msg);
+        } catch (IOException ex) {
+            Assert.fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void encodesPutRequestCorrectly_2() {
+        // What erlang generates
+        // A = riakc_ts_put_operator:serialize(<<"test_table">>,[{<<"series">>, <<"family">>, 12345678, 1, true, 34.3, []}], true).
+        // A = {tsputreq,<<"test_table">>,[],[{<<"series">>,<<"family">>,12345678,1,true,34.3,[]}]}
+        // rp(term_to_binary(A)). => exp array below
+
+        // # What we generate
+        // B = <<-125, 104, 4, 100, 0, 8, 116, 115, 112, 117, 116, 114, 101, 113, 109, 0, 0, 0, 10, 116, 101, 115, 116, 95, 116, 97, 98, 108, 101, 106, 108, 0, 0, 0, 1, 104, 7, 109, 0, 0, 0, 6, 115, 101, 114, 105, 101, 115, 109, 0, 0, 0, 6, 102, 97, 109, 105, 108, 121, 98, 0, -68, 97, 78, 97, 1, 100, 0, 4, 116, 114, 117, 101, 70, 64, 65, 38, 102, 102, 102, 102, 102, 106, 106, 106>>.
+        // C = binary_to_term(B).
+        // C = {tsputreq,<<"test_table">>,[],[{<<"series">>,<<"family">>,12345678,1,true,34.3,[]}]}
+        // A = C. #true (wtf) (magic)
+
+        final byte[] exp = {(byte)131,104,4,100,0,8,116,115,112,117,116,114,101,113,109,0,
+                            0,0,10,116,101,115,116,95,116,97,98,108,101,106,108,0,0,
+                            0,1,104,7,109,0,0,0,6,115,101,114,105,101,115,109,0,0,0,
+                            6,102,97,109,105,108,121,98,0,(byte)188,97,78,97,1,100,0,4,
+                            116,114,117,101,99,51,46,52,50,57,57,57,57,57,57,57,57,
+                            57,57,57,57,57,55,49,53,55,56,101,43,48,49,0,0,0,0,0,
+                            106,106};
+
+        final ArrayList<Row> rows = new ArrayList<>(1);
+        rows.add(new Row(new Cell("series"), new Cell("family"), Cell.newTimestamp(12345678),
+                         new Cell(1L), new Cell(true), new Cell(34.3), null));
+
+        try {
+            OtpOutputStream os = TermToBinaryCodec.encodeTsPutRequest(TABLE_NAME, rows);
+            os.flush();
+            byte[] msg = os.toByteArray();
+            Assert.assertArrayEquals(exp, msg);
+        } catch (IOException ex) {
+            Assert.fail(ex.getMessage());
+        }
+    }
 
     @Test
     public void encodesGetRequestCorrectly() {
@@ -54,41 +130,6 @@ public class TermToBinaryCodecTest
 
         try {
             OtpOutputStream os = TermToBinaryCodec.encodeTsQueryRequest(QUERY);
-            os.flush();
-            byte[] msg = os.toByteArray();
-            Assert.assertArrayEquals(exp, msg);
-        } catch (IOException ex) {
-            Assert.fail(ex.getMessage());
-        }
-    }
-
-    @Test
-    public void encodesPutRequestCorrectly() {
-        // What erlang generates
-        // A = riakc_ts_put_operator:serialize(<<"test_table">>,[{<<"series">>, <<"family">>, 12345678, 1, true, 34.3, []}], true).
-        // A = {tsputreq,<<"test_table">>,[],[{<<"series">>,<<"family">>,12345678,1,true,34.3,[]}]}
-        // rp(term_to_binary(A)). => exp array below
-
-        // # What we generate
-        // B = <<-125, 104, 4, 100, 0, 8, 116, 115, 112, 117, 116, 114, 101, 113, 109, 0, 0, 0, 10, 116, 101, 115, 116, 95, 116, 97, 98, 108, 101, 106, 108, 0, 0, 0, 1, 104, 7, 109, 0, 0, 0, 6, 115, 101, 114, 105, 101, 115, 109, 0, 0, 0, 6, 102, 97, 109, 105, 108, 121, 98, 0, -68, 97, 78, 97, 1, 100, 0, 4, 116, 114, 117, 101, 70, 64, 65, 38, 102, 102, 102, 102, 102, 106, 106, 106>>.
-        // C = binary_to_term(B).
-        // C = {tsputreq,<<"test_table">>,[],[{<<"series">>,<<"family">>,12345678,1,true,34.3,[]}]}
-        // A = C. #true (wtf) (magic)
-
-        final byte[] exp = {(byte)131,104,4,100,0,8,116,115,112,117,116,114,101,113,109,0,
-                            0,0,10,116,101,115,116,95,116,97,98,108,101,106,108,0,0,
-                            0,1,104,7,109,0,0,0,6,115,101,114,105,101,115,109,0,0,0,
-                            6,102,97,109,105,108,121,98,0,(byte)188,97,78,97,1,100,0,4,
-                            116,114,117,101,99,51,46,52,50,57,57,57,57,57,57,57,57,
-                            57,57,57,57,57,55,49,53,55,56,101,43,48,49,0,0,0,0,0,
-                            106,106};
-
-        final ArrayList<Row> rows = new ArrayList<>(1);
-        rows.add(new Row(new Cell("series"), new Cell("family"), Cell.newTimestamp(12345678),
-                         new Cell(1L), new Cell(true), new Cell(34.3), null));
-
-        try {
-            OtpOutputStream os = TermToBinaryCodec.encodeTsPutRequest(TABLE_NAME, rows);
             os.flush();
             byte[] msg = os.toByteArray();
             Assert.assertArrayEquals(exp, msg);
