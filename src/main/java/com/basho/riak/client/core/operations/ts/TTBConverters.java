92e31eafe61b6e3d1d53d8c15b7c10999c7c7b02
Luke Bakken <luke@bakken.io>
2016-04-21 11:17:00 -0700

    Remove the double-header from TTB data.

--- src/main/java/com/basho/riak/client/core/operations/ts/TTBConverters.java
+++ src/main/java/com/basho/riak/client/core/operations/ts/TTBConverters.java
@@ -4,13 +4,9 @@ import com.basho.riak.client.core.codec.TermToBinaryCodec;
 import com.basho.riak.client.core.operations.TTBFutureOperation;
 import com.basho.riak.client.core.query.timeseries.Cell;
 import com.basho.riak.client.core.query.timeseries.QueryResult;
-import com.basho.riak.protobuf.RiakMessageCodes;
 import com.ericsson.otp.erlang.OtpErlangDecodeException;
 import com.ericsson.otp.erlang.OtpOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,7 +15,6 @@ class TTBConverters
 {
     private static abstract class MemoizingEncoder<T> implements TTBFutureOperation.TTBEncoder
     {
-        // private final byte reqMessageCode = RiakMessageCodes.MSG_TsTtbMsg;
         protected byte[] message = null;
         protected final T builder;
 
@@ -38,20 +33,7 @@ class TTBConverters
                 try {
                     OtpOutputStream os = buildMessage();
                     os.flush();
-                    
-                    // TODO GH-611 should the output stream or base type be returned?
-                    // TODO GH-611 we still need to add msgCode 141 and message length
-                    final int msgLen = os.length() + 5; // Add 4-byte msg length and 1-byte msgCode
-                    ByteArrayOutputStream bs = new ByteArrayOutputStream(msgLen);
-                    
-                    DataOutputStream ds = new DataOutputStream(bs);
-                    ds.writeInt(msgLen);
-                    ds.writeByte(RiakMessageCodes.MSG_TsTtbMsg);
-                    ds.flush();
-                    
-                    os.writeTo(bs);
-                    bs.flush();
-                    message = bs.toByteArray();
+                    message = os.toByteArray();
                 } catch (IOException ex) {
                     // TODO GH-611
                     Logger.getLogger(TTBConverters.class.getName()).log(Level.SEVERE, null, ex);
