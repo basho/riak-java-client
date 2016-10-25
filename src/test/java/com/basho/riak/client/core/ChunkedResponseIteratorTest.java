3ac537cf690e974b1b94f9f34537ec0b5d4df4a1
Alex Moore <moore.alex@gmail.com>
2016-10-25 01:04:17 -0400

    Move test to another thread, as to not upset other Interruptable tests on the main test thread

--- src/test/java/com/basho/riak/client/core/ChunkedResponseIteratorTest.java
+++ src/test/java/com/basho/riak/client/core/ChunkedResponseIteratorTest.java
@@ -24,43 +24,55 @@ public class ChunkedResponseIteratorTest
       https://bugs.openjdk.java.net/browse/JDK-8154017
      */
     @Test
-    public void testInterruptedExceptionDealtWith()
+    public void testInterruptedExceptionDealtWith() throws InterruptedException
     {
-        boolean caught = false;
-        InterruptedException ie = null;
+        final boolean[] caught = {false};
+        final InterruptedException[] ie = {null};
         int timeout = 1000;
 
-        try
+        Thread t = new Thread(() ->
         {
-            @SuppressWarnings("unchecked")
-            TransferQueue<FakeResponse> fakeQueue = (TransferQueue<FakeResponse>)mock(TransferQueue.class);
-            when(fakeQueue.poll(timeout, TimeUnit.MILLISECONDS)).thenThrow(new InterruptedException("foo"));
+            try
+            {
+                @SuppressWarnings("unchecked") TransferQueue<FakeResponse> fakeQueue =
+                        (TransferQueue<FakeResponse>) mock(TransferQueue.class);
+                when(fakeQueue.poll(timeout,
+                                    TimeUnit.MILLISECONDS)).thenThrow(new InterruptedException(
+                        "foo"));
 
-            @SuppressWarnings("unchecked")
-            StreamingFutureOperation<FakeResponse, Void, Void> coreFuture =
-                    (StreamingFutureOperation<FakeResponse, Void, Void>)mock(StreamingFutureOperation.class);
+                @SuppressWarnings("unchecked") StreamingFutureOperation<FakeResponse, Void, Void>
+                        coreFuture = (StreamingFutureOperation<FakeResponse, Void, Void>) mock(
+                        StreamingFutureOperation.class);
 
-            when(coreFuture.getResultsQueue()).thenReturn(fakeQueue);
+                when(coreFuture.getResultsQueue()).thenReturn(fakeQueue);
 
-            // ChunkedResponseIterator polls the response queue when created,
-            // so we'll use that to simulate a Thread interrupt.
-            new ChunkedResponseIterator<>(coreFuture, timeout, Long::new, FakeResponse::iterator);
-        }
-        catch (RuntimeException ex)
-        {
-            caught = true;
-            ie = (InterruptedException)ex.getCause();
-        }
-        catch (InterruptedException e)
-        {
-            // Mocking TransferQueue::poll(timeout) requires this CheckedException be dealt with
-            // If we actually catch one here we've failed at our jobs.
-            caught = false;
-        }
+                // ChunkedResponseIterator polls the response queue when created,
+                // so we'll use that to simulate a Thread interrupt.
+                new ChunkedResponseIterator<>(coreFuture,
+                                              timeout,
+                                              Long::new,
+                                              FakeResponse::iterator);
+            }
+            catch (RuntimeException ex)
+            {
+                caught[0] = true;
+                ie[0] = (InterruptedException) ex.getCause();
+            }
+            catch (InterruptedException e)
+            {
+                // Mocking TransferQueue::poll(timeout) requires this CheckedException be dealt with
+                // If we actually catch one here we've failed at our jobs.
+                caught[0] = false;
+            }
 
-        assertTrue(caught);
-        assertEquals("foo", ie.getMessage());
-        assertTrue(Thread.currentThread().isInterrupted());
+            assertTrue(Thread.currentThread().isInterrupted());
+        });
+
+        t.start();
+        t.join();
+
+        assertTrue(caught[0]);
+        assertEquals("foo", ie[0].getMessage());
     }
 
     static abstract class FakeResponse implements Iterable<Integer> {}
