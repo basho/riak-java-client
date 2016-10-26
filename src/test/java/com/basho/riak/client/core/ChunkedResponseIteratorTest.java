1c5883c92420561e38e3ef2a2b216e181db48f2c
Alex Moore <moore.alex@gmail.com>
2016-10-26 10:33:58 -0400

    Fix test that stubs a final method

--- src/test/java/com/basho/riak/client/core/ChunkedResponseIteratorTest.java
+++ src/test/java/com/basho/riak/client/core/ChunkedResponseIteratorTest.java
@@ -3,18 +3,23 @@ package com.basho.riak.client.core;
 
 import com.basho.riak.client.api.commands.ChunkedResponseIterator;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TransferQueue;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 /**
  * @author Alex Moore <amoore at basho dot com>
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PBStreamingFutureOperation.class)
 public class ChunkedResponseIteratorTest
 {
     /*
@@ -26,23 +31,22 @@ public class ChunkedResponseIteratorTest
     @Test
     public void testInterruptedExceptionDealtWith() throws InterruptedException
     {
-        final boolean[] caught = {false};
-        final InterruptedException[] ie = {null};
+        final Throwable[] ex = {null};
         int timeout = 1000;
 
-        Thread t = new Thread(() ->
+        Thread testThread = new Thread(() ->
         {
             try
             {
-                @SuppressWarnings("unchecked") TransferQueue<FakeResponse> fakeQueue =
+                @SuppressWarnings("unchecked")
+                TransferQueue<FakeResponse> fakeQueue =
                         (TransferQueue<FakeResponse>) mock(TransferQueue.class);
-                when(fakeQueue.poll(timeout,
-                                    TimeUnit.MILLISECONDS)).thenThrow(new InterruptedException(
-                        "foo"));
 
-                @SuppressWarnings("unchecked") PBStreamingFutureOperation<FakeResponse, Void, Void>
-                        coreFuture = (PBStreamingFutureOperation<FakeResponse, Void, Void>) mock(
-                        PBStreamingFutureOperation.class);
+                when(fakeQueue.poll(timeout, TimeUnit.MILLISECONDS)).thenThrow(new InterruptedException("foo"));
+
+                @SuppressWarnings("unchecked")
+                PBStreamingFutureOperation<FakeResponse, Void, Void> coreFuture =
+                        (PBStreamingFutureOperation<FakeResponse, Void, Void>) mock(PBStreamingFutureOperation.class);
 
                 when(coreFuture.getResultsQueue()).thenReturn(fakeQueue);
 
@@ -53,26 +57,30 @@ public class ChunkedResponseIteratorTest
                                               Long::new,
                                               FakeResponse::iterator);
             }
-            catch (RuntimeException ex)
+            catch (RuntimeException rex)
             {
-                caught[0] = true;
-                ie[0] = (InterruptedException) ex.getCause();
+                ex[0] = rex;
             }
             catch (InterruptedException e)
             {
                 // Mocking TransferQueue::poll(timeout) requires this CheckedException be dealt with
                 // If we actually catch one here we've failed at our jobs.
-                caught[0] = false;
+                fail(e.getMessage());
             }
 
             assertTrue(Thread.currentThread().isInterrupted());
         });
 
-        t.start();
-        t.join();
+        testThread.start();
+        testThread.join();
 
-        assertTrue(caught[0]);
-        assertEquals("foo", ie[0].getMessage());
+        Throwable caughtException = ex[0];
+        assertNotNull(caughtException);
+
+        Throwable wrappedException = caughtException.getCause();
+        assertNotNull(caughtException.getMessage(), wrappedException);
+        assertEquals("foo", wrappedException.getMessage());
+        assertTrue(wrappedException instanceof InterruptedException);
     }
 
     static abstract class FakeResponse implements Iterable<Integer> {}
